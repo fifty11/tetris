88e1b6bf3aae8c59fab4cf539be72d6f9bd29435
sixten <sixteenvolts@proton.me>
2023-12-22 01:17:56 +0100

    tetriz

--- main.c
+++ main.c
@@ -1,26 +1,18 @@
-#include "stdbool.h"
-#include "tetris.h"
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-
-#define RIGHT 0
-#define LEFT 1
-
-#define L 0
-#define BACKWARDS_L 1
-#define BAR 2
-#define SQUARE 3
-#define PYRAMID 4
+#include "tetris.h"
 
 char game;
 
+uint16_t tetrinos[] = {0x0644, 0x0622, 0x4444, 0x0066, 0x00E4};
+
 void turn(uint16_t *tetrino, char dir);
+
 
 int main(int argc, char **argv) {
 	// constants:
-	uint16_t tetrinos[] = {0x0644, 0x0622, 0x4444, 0x6600, 0xE400};
 	struct Tetrinos {
 		uint16_t current;
 		uint16_t saved;
@@ -35,16 +27,74 @@ int main(int argc, char **argv) {
 	// game
 	if (argc > 1)
 		seed = atoi(argv[0]);
+	
 
-	return 0;
+	printf("pyramid:\n");
+	for (int i = 0; i < 16; ++i) {
+		if (PYRAMID & (0x01 << i))
+			printf("1 ");
+		else
+			printf("0 ");
+
+		if ((i + 1) % 4 == 0)
+			printf("\n");
+	}
+
+	printf("\nL:\n");
+	for (int i = 0; i < 16; ++i) {
+		if (L & (0x01 << i))
+			printf("1 ");
+		else
+			printf("0 ");
+
+		if ((i + 1) % 4 == 0)
+			printf("\n");
+	}
+
+	printf("\nbackwardsL:\n");
+	for (int i = 0; i < 16; ++i) {
+		if (BACKWARDS_L& (0x01 << i))
+			printf("1 ");
+		else
+			printf("0 ");
+
+		if ((i + 1) % 4 == 0)
+			printf("\n");
+	}
+
+	printf("\nbar:\n");
+	for (int i = 0; i < 16; ++i) {
+		if (BAR & (0x01 << i))
+			printf("1 ");
+		else
+			printf("0 ");
+
+		if ((i + 1) % 4 == 0)
+			printf("\n");
+	}
+
+	printf("\nsquare:\n");
+	for (int i = 0; i < 16; ++i) {
+		if (SQUARE & (0x01 << i))
+			printf("1 ");
+		else
+			printf("0 ");
+
+		if ((i + 1) % 4 == 0)
+			printf("\n");
+	}
+
+	return 0; // return statment more info:
+						// https://www.geeksforgeeks.org/return-statement-in-c/
 }
 
 void turn(uint16_t *tetrino, char dir) {
 	uint16_t tempTrino = 0x00;
 	if (dir) {
-		char sector; // sector is which fourth of the BIT field the function is processing
-		*tetrino=tempTrino;
+		char sector; // sector is which fourth of the BIT field the function is
+								 // insert processing
+		*tetrino = tempTrino;
 		return;
 	}
-	*tetrino=tempTrino;
+	*tetrino = tempTrino;
 }
--- tetris.h
+++ tetris.h
@@ -1,6 +1,16 @@
 #ifndef TETRIS_HEADER_GUARD
 #define TETRIS_HEADER_GUARD
 
+#define RIGHT 0
+#define LEFT 1
+
+#define L tetrinos[0]
+#define BACKWARDS_L tetrinos[1]
+#define BAR tetrinos[2]
+#define SQUARE tetrinos[3]
+#define PYRAMID tetrinos[4]
+
+
 void display(void);
 void keyPress(void);
 void logic(void);
